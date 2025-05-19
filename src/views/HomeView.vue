b1d528364054fc75b86e6e5986dfd5a9c9823869
Ramiro <ramiatorres@outlook.com>
2025-05-19 18:32:59 +0200

    Initial commit: project setup and first three progress blocks

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Home View</h1>
+    <p v-if="user">Welcome, {{ user.email }}</p>
+    <button @click="setUserExample">Set user</button>
+    <button @click="clearUser">Clear user</button>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/store/user'
+import { storeToRefs } from 'pinia'
+
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
+
+function setUserExample() {
+  userStore.setUser({
+    id: '123',
+    email: 'test@example.com'
+  })
+}
+
+function clearUser() {
+  userStore.clearUser()
+}
+</script>
